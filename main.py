f9ea04d4a1eafb1f2ea82ea6496c5d6df0c456e2
Ann B <47284102+itsannb@users.noreply.github.com>
2021-04-13 12:18:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 # Import discord package
 import discord
+from discord.ext import commands
 
 # The bot
-bot = discord.Client()
+bot = commands.Bot(command_prefix='m! ')
+
+@bot.command(name = 'motivate')
+async def motivate(context):
+    
+    await context.message.channel.send('You can do this! I believe in you!')
+@bot.command(name = 'version')
+async def version(context):
+
+    myEmbed = discord.Embed(title = "Current Version", description = "The bot is in version 1.0", color = 0x00ff00) # hexadecimal for number color
+    myEmbed.add_field(name = "Version Code: ", value = "v1.0.0", inline = False)
+    myEmbed.add_field(name = "Date Released: ", value = "April 5th, 2020", inline = False)
+    myEmbed.set_footer(text = "itsannb production")
+    myEmbed.set_author(name = "Ann B")
+
+    await context.message.channel.send(embed = myEmbed)
 
 # creates a function
 @bot.event
@@ -19,17 +35,18 @@ async def on_message(message):
         myEmbed = discord.Embed(title = "Current Version", description = "The bot is in version 1.0", color = 0x00ff00) # hexadecimal for number color
         myEmbed.add_field(name = "Version Code: ", value = "v1.0.0", inline = False)
         myEmbed.add_field(name = "Date Released: ", value = "April 5th, 2020", inline = False)
-        myEmbed.set_footer(text = "This is a sample footer")
+        myEmbed.set_footer(text = "itsannb production")
         myEmbed.set_author(name = "Ann B")
 
         await general_channel.send(embed = myEmbed)
+    
+    await bot.process_commands(message)
 
 # When the bot disconnects from the server
 @bot.event
 async def on_disconnet():
     general_channel = bot.get_channel(827643671171039286)
     await general_channel.send('Goodbye and good luck in your assignments!')
-
 
 # Run the bot on server
 bot.run('token')
